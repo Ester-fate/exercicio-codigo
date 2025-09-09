6a7607abcea0b7ed6177c664638a7ca0843872a8
Ester <amorimnubia5@gmail.com>
2025-09-09 09:15:45 -0300

    Create atividade4.py

---
+++ atividade4.py
@@ -0,0 +1,8 @@
+horario=input("Digite a hora do momento exato")
+dia=6.0
+noite=18.01
+if(horario<=noite&&horario>01.00)
+    print("Agora e dia")
+if(horario>=dia&&horario<23.59)
+    print("Agora e noite")
+else print("Digite um horario valido")
