eb76412fbc5c7086d5dea0f4d9e465bd52efc0b9
Ester <amorimnubia5@gmail.com>
2025-09-09 09:16:04 -0300

    Create atividade6.py

---
+++ atividade6.py
@@ -0,0 +1,14 @@
+"Crie um programa que faz a troca de um valor."
+"Usando apenas três variáveis."
+
+Tipo1 = "Kat"
+Tipo2 = "Et" 
+Tipo3 = "Cad"
+print("Antes da troca", Tipo1, Tipo2, Tipo3)
+
+aux = Tipo1
+Tipo1 = Tipo2
+Tipo2 = Tipo3
+Tipo3 = aux
+
+print("Depois da troca de variveis", Tipo1, Tipo2, Tipo3)
