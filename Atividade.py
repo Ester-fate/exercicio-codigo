d695fe563cd2a593db797feb4bb6d29b97c87276
Ester <amorimnubia5@gmail.com>
2025-09-09 09:15:15 -0300

    Create Atividade.py

---
+++ Atividade.py
@@ -0,0 +1,20 @@
+"""
+Faça um programa que peça um número e então mostre a mensagem.
+O número informado é [ número ].
+ variável = input - entrada do número.
+ mostrar na tela
+
+numero = input("Digite um numero:")
+print("numero informado é:", numero)
+
+"""
+
+Numero1 = input("Digite o primeiro numero inteiro:")
+Numero2 = input("Digite o segundo numero inteiro:")
+soma=  int(Numero1) + int(Numero2)
+print("A soma dos numeros é:", soma)
+
+"""
+Faça um programa que peça dois numeros inteiros e imprima a soma desses dois numeros.
+
+"""
