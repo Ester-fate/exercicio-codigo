ec8be18c6ec42228c63cc57c95ad6e0ef47c2402
Ester <amorimnubia5@gmail.com>
2025-09-09 09:16:22 -0300

    Create atividade7.py

---
+++ atividade7.py
@@ -0,0 +1,10 @@
+"Crie um programa que some dois numeros, guarde o valor e depois some com o somaresultado da soma anterior."
+
+numero1 = input("Digte o primeiro numero:")
+numero2 = input("Digite o segundo numero:")
+soma = int(numero1) + int(numero2)
+print("A soma dos numeros é:", soma)
+
+numero3 = input("Digite o terceiro numero:")
+s2 = int(soma) + int(numero3) 
+print("O valor da soma é:", s2) 
