b53c34fd242dc6b33863c7fb381e0d576ec36e55
Ester <amorimnubia5@gmail.com>
2025-09-09 09:15:32 -0300

    Create Atividade2.py

---
+++ Atividade2.py
@@ -0,0 +1,9 @@
+"""
+Faça um programa que peça as 4 notas bimestrais e mostre a média.
+"""
+Nota1 = input("Digite a primeira nota inteiro:")
+Nota2 = input("Digite a segunda nota inteiro:")
+Nota3 = input("Digite a terceira nota inteiro:")
+Nota4 = input("Digite a quarta nota inteiro:")
+media = int(Nota1)+int(Nota2)+int(Nota3)+int(Nota4)/4
+print("A media das notas é:", media)
